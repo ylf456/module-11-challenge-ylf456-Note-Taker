5116be083711ad9b83f7ad285103722711f7c0fd
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-01-03 18:00:07 -0800

    add express dependecy

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-const PORT = process.env.PORT || 3001;
-const app = express();
 const api = require('./routes/index.js');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 app.use(express.json());
